53a8e0526ce525ab321c242ff84c74ac6e2630d8
Olti Berbatovci <oltiberbatovci76@gmail.com>
2025-01-28 13:44:07 +0100

    Implemented Order page validation in backend with database insertion

--- order.php
+++ order.php
@@ -1,3 +1,33 @@
+<?php
+include_once 'Rezervimi.php';
+include_once 'RezervimiRepository.php';
+
+if (isset($_POST['submit'])) {
+    $emri = $_POST['emri'];
+    $email = $_POST['email'];
+    $nrkontaktues = $_POST['Nrcontact'];
+    $vendndodhja = $_POST['location'];
+
+    $errors = array();
+
+    if(empty($emri) || empty($email) || empty($nrkontaktues) || empty($vendndodhja)){
+            $errors[] = "All fields are required!";
+        }else{
+    $Rezervim = new Rezervimi($emri, $email, $nrkontaktues,$vendndodhja);
+    $rezervimiRepository = new RezervimiRepository();
+    $rezervimiRepository->insertRezervimet($Rezervim);
+    header("location:order.php");
+}
+}
+?>
+<?php if (!empty($errors)) { ?>
+    <div class="error-message" id="allFieldsError">
+        <?php foreach ($errors as $error) {
+            echo $error . "<br>";
+        } ?>
+    </div>
+<?php } ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,33 +37,33 @@
     <link rel="stylesheet" href="order.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
- 
+  
     <title>YOUR ORDER</title>
 </head>
 <body>
     <section id="nav-bar">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-              <a class="navbar-logo" href="index.html">ThirreTaxin</a>
+              <a class="navbar-logo" href="index.php">ThirreTaxin</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
               </button>
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link" href="index.html">HOME</a>
+                    <a class="nav-link" href="index.php">HOME</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="about.html">ABOUT</a>
+                    <a class="nav-link" href="about.php">ABOUT</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="order.html">POROSIT</a>
+                    <a class="nav-link" href="order.php">POROSIT</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="contact.html">CONTACT</a>
+                    <a class="nav-link" href="contact.php">CONTACT</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="punesimi.html">PUNESOHU</a>
+                    <a class="nav-link" href="punesimi.php">PUNESOHU</a>
                   </li>
                 </ul>
                 <a href="login.html"><button id="login-btn">Login</button></a>
@@ -49,25 +79,25 @@
                <h3>REZERVO UDHETIMIN TEND</h3>
 
                <div class="input-field">
-                <!-- <p style="font-size: 16px;">Emri dhe Mbiemri</p> -->
+                <p style="font-size: 16px;">Emri dhe Mbiemri</p>
                <input type="text" name="emri"  id="emri" placeholder="Shkruaj emrin">
                <div class="error-message" id="emriError"></div>
             </div>
          
             <div class="input-field">
-               <!-- <p style="font-size: 16px;">Email</p> -->
+               <p style="font-size: 16px;">Email</p>
                <input type="email" name="email" id="email"  placeholder="Shkruaj email-in">
                <div class="error-message" id="emailError"></div>
             </div>
 
             <div class="input-field">
-               <!-- <p style="font-size: 16px;">Numri kontaktues</p> -->
+               <p style="font-size: 16px;">Numri kontaktues</p>
                 <input id="Nrcontact" type="number" name="Nrcontact" placeholder="Shkruaj numrin e telefonit:">
                 <div class="error-message" id="NrcontactError"></div>
             </div>
 
             <div class="input-field">
-                <p style="font-size: 16px; justify-content: center;;">Shkruaj vendndodhjen tuaj</p>
+                <p style="font-size: 16px;">Shkruaj vendndodhjen tuaj</p>
                 <input id="location" type="text" name="location" placeholder="Tregoni ku gjendeni:">
             <div class="error-message" id="locationError"></div>
             </div>
@@ -82,7 +112,7 @@
                 <option value="blue">Blue TAXI</option>
               </select>
 
-              <p style="justify-content: center; font-size:13px;"><small>Detajet e porosise do t'i pranoni me email.</small></p>
+              <p style="justify-content: center;"><small>Detajet e porosise do t'i pranoni me email.</small></p>
 
             <button name="submit"class="porosia-butoni" type="submit" onclick="return validateForm()"> Porosit</button>        
         </form>
@@ -92,21 +122,21 @@
       function validateForm(){
           let emri=document.getElementById('emri').value;
           let emriError=document.getElementById('emriError');
- 
+  
           let email=document.getElementById('email').value;
           let emailError=document.getElementById('emailError');
 
           let Nrcontact=document.getElementById('Nrcontact').value;
           let NrcontactError=document.getElementById('NrcontactError');
-         
+          
           let location=document.getElementById('location').value;
           let locationError=document.getElementById('locationError');
- 
+  
           emriError.innerText='';
           emailError.innerText='';
           NrcontactError.innerText='';
           locationError.innerText='';
- 
+  
           let emriRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
               if(emri.trim()==""||!emriRegex.test(emri)){
                 emriError.innerText="Emri eshte invalid";
@@ -125,10 +155,8 @@
               return true;
       }
   </script>
- 
+  
                 <table class="table">
-
-                    <h1 style="text-align: center; margin-top: 30px;">CMIMORE</h1>
                     <thead class="thead-dark">
                       <tr>
                         <th scope="col">#</th>
@@ -200,8 +228,8 @@
                       </tr>
                     </tbody>
                   </table>
-                 
-                 
+                  
+                  
 
     </main>
     <footer>
@@ -209,15 +237,15 @@
             <a class="footer-logo" href="index.html">ThirreTaxin</a>
             <p class="p2-description" style="width: 40%; padding: 20px;">Zgjedhja juaj sapo eshte bere me e lehte. Me ne do te keni mundesine te kurseni kohen tuaj. Shpejte, sigurt dhe efikas. Te gjitha ne nje vend!
             Platforma me e re ne treg e flotes se taksive me gjithsej 6 operatore nen sherbimin tuaj 24/7.</p>
-       
-   
+        
+    
         <div class="footer-center">
             <h4>Links</h4>
             <a href="https://www.facebook.com/">Facebook</a>
             <a href="https://www.instagram.com/">Instagram</a>
             <a href="https://www.google.com/">Google</a>
         </div>
-   
+    
         <div class="footer-right">
             <h4>Contact</h4>
                 <p>044 123 123</p>
@@ -235,7 +263,7 @@
             let imgArray =['blue-taxi2.png','e-taxi-tesla.png','golden-taxi.png','online-taxi.png','Pink-taxi.png','urban-taxi.png'];
        
             function changeImg(){
-               
+                
                 document.getElementById('slideshow').scr=imgArray[i];
                     if(i<imgArray.length-1){
                         i++;
@@ -261,7 +289,7 @@
     let locationError = document.getElementById('locationError');
     let allFieldsError = document.getElementById('allFieldsError');
 
-    let hasErrors = false;
+    let hasErrors = false; 
 
     emriError.innerText = '';
     emailError.innerText = '';
@@ -298,7 +326,7 @@
         hasErrors = true;
         return false;
     }
-   
+    
     if (emri.trim() === "" || email.trim() === "" || Nrcontact.trim() === "" || location.trim() === "") {
         hasErrors = true;
         return false;
@@ -309,7 +337,7 @@
     }
     return true;
 
-}  
+}   
        </script>
 </body>
 </html>
